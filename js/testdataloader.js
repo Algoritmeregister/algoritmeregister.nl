9a4ef9eb907588f14fbe2e76e04095a98500d9ce
Johan Groenen <johan.groenen@gmail.com>
2022-10-23 14:40:09 +0200

    add search description

--- js/testdataloader.js
+++ js/testdataloader.js
@@ -2,11 +2,13 @@ import DataLoaderDenHaag from './modules/DataLoaderDenHaag.mjs';
 import DataLoaderUtrecht from './modules/DataLoaderUtrecht.mjs';
 import Algoritmeregister from './modules/Algoritmeregister.mjs';
 
-var dataLoaderDenHaag = new DataLoaderDenHaag();
-var dataLoaderUtrecht = new DataLoaderUtrecht();
 
 var algoritmeregister = new Algoritmeregister();
+
+var dataLoaderDenHaag = new DataLoaderDenHaag();
 algoritmeregister.addData(await dataLoaderDenHaag.getData());
+
+var dataLoaderUtrecht = new DataLoaderUtrecht();
 algoritmeregister.addData(await dataLoaderUtrecht.getData());
 
 document.getElementById("search-bar").onkeyup = function () { algoritmeregister.filter(this.value); };

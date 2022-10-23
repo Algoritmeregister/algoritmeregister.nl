d0fde7c411f47934db0a1e0eb7cd5e550292151c
Johan Groenen <johan.groenen@gmail.com>
2022-10-23 14:08:55 +0200

    add super basic filter-search

--- js/modules/Algoritmeregister.mjs
+++ js/modules/Algoritmeregister.mjs
@@ -1,6 +1,10 @@
 export default function () {
     var data = [];
 
+    function addData(orgData) {
+        data = data.concat(orgData);
+    }
+
     function render() {
         document.getElementById("search-results").innerHTML = data.map(item => tmpl("search_result_tmpl", item)).join("");
         document.getElementById("numberOfSearchResults").innerHTML = data.length;
@@ -8,13 +12,23 @@ export default function () {
         //console.log(algorithmTypes); // FIXME TO-DO
     }
 
-    function addData(orgData) {
-        data = data.concat(orgData);
+    function filter(str) {
+        var matches = 0;
+        document.querySelectorAll('[data-search-text]').forEach(function (item) {
+            if (item.dataset.searchText.indexOf(str.toLowerCase()) == -1) {
+                item.style.display = "none";
+            } else {
+                item.style.display = "block";
+                matches++;
+            }
+        });
+        document.getElementById("numberOfSearchResults").innerHTML = matches;
     }
 
     return {
         addData,
-        render
+        render,
+        filter
     }
 
 }
--- js/testdataloader.js
+++ js/testdataloader.js
@@ -9,4 +9,6 @@ var algoritmeregister = new Algoritmeregister();
 algoritmeregister.addData(await dataLoaderDenHaag.getData());
 algoritmeregister.addData(await dataLoaderUtrecht.getData());
 
+document.getElementById("search-bar").onkeyup = function () { algoritmeregister.filter(this.value); };
+
 algoritmeregister.render();
